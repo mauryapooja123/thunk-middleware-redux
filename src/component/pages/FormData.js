5caa4fb123a23dd75d408ef2a7f021afc9f18219
poojaEminence <poojamaurya.eminence@gmail.com>
2022-05-09 09:13:31 +0530

    first commit

---
+++ src/component/pages/FormData.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const FormData = () => {
+  return (
+    <>
+      <p style={{ background: "lightblue" }}>
+        A paragraph is a series of related sentences developing a central idea,
+        called the topic. Try to think about paragraphs in terms of thematic
+        unity:
+      </p>
+      <h3 style={{ color: " red " }}>
+        a paragraph is a sentence or a group of sentences that supports one
+        central, unified idea. Paragraphs add one idea at a time to your broader
+        argument.iuoooo
+      </h3>
+      <img src={`${process.env.REACT_APP_P}`} alt="lllllllllll" width="500px" />
+    </>
+  );
+};
+export default FormData;
